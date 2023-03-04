8a0baf4af692321801cf234115df93bf5608ef43
Trevor French <trevorf96@gmail.com>
2023-03-04 14:31:57 -0700

    working on docstrings

--- R/functions.R
+++ R/functions.R
@@ -39,43 +39,73 @@ ab_fr <- function(dataset, col_name) {
   col_name <- dplyr::enquo(col_name)
   dataset |>
     dplyr::group_by(!!col_name) |>
-    dplyr::summarise(absolute_frequency = n())
+    dplyr::summarise(absolute_frequency = dplyr::n())
 }
 
-
-
-## 3 - Relative frequency
+#' re_fr
+#'
+#' @param dataset the dataset for which you wish to retrieve relative frequency.
+#' @param col_name the column name for which you wish to retrieve relative frequency.
+#'
+#' @return returns the relative frequency for a specified column.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' re_fr(dataset = soccer, col_name =  result_19_20)}
 re_fr <- function(dataset, col_name) {
   col_name <- dplyr::enquo(col_name)
   dataset |>
     dplyr::group_by(!!col_name) |>
-    dplyr::summarise(total = n()) |>
+    dplyr::summarise(total = dplyr::n()) |>
     dplyr::mutate (relative_frequency = round(100 * (total / sum(total)), 2)) |>
     dplyr::select(!!col_name, relative_frequency)
 }
 
-re_fr(dataset = df, col_name =  result_19_20)
-
-## 4 - Full frequency (absolute and relative)
+#' full_fr
+#'
+#' @param dataset the dataset for which you wish to retrieve full frequency.
+#' @param col_name the column name for which you wish to retrieve full frequency.
+#'
+#' @return returns the full frequency (absolute and relative) for a specified column.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' full_fr(dataset = df, col_name = result_19_20)}
 full_fr <- function(dataset, col_name) {
   col_name <- dplyr::enquo(col_name)
   dataset |>
     dplyr::group_by(!!col_name) |>
-    dplyr::summarise(absolute_frequency = n()) |>
+    dplyr::summarise(absolute_frequency = dplyr::n()) |>
     dplyr::mutate (relative_frequency = round(100 * (
       absolute_frequency / sum(absolute_frequency)
     ), 2))
-  
 }
 
-full_fr(dataset = df, col_name = result_19_20)
-
-## 5 - Comparative of the absolute frequency for the a variable in different moments
+#' co_ab_fr
+#'
+#' @param dataset the dataset for which you wish to retrieve comparative 
+#' absolute frequency.
+#' @param col_name_1 the first column name for which you wish to retrieve 
+#' comparative absolute frequency.
+#' @param col_name_2 the second column name for which you wish to retrieve 
+#' comparative absolute frequency.
+#'
+#' @return returns the comparative of the absolute frequency for the a variable 
+#' in different moments.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#'co_ab_fr(dataset = soccer,
+#'         col_name_1 = result_19_20,
+#'         col_name_2 = result_20_21)}
 co_ab_fr <- function(dataset, col_name_1, col_name_2) {
   col_name_1 <- dplyr::enquo(col_name_1)
   col_name_2 <- dplyr::enquo(col_name_2)
   values <- dataset |> dplyr::group_by(!!col_name_1) |>
-    dplyr::summarise(n = n()) |> dplyr::select(-2) |>
+    dplyr::summarise(n = dplyr::n()) |> dplyr::select(-2) |>
     dplyr::rename(values = !!col_name_1)
   
   dataset |>
@@ -88,16 +118,29 @@ co_ab_fr <- function(dataset, col_name_1, col_name_2) {
     dplyr::mutate(dif_abs = abs(!!col_name_1-!!col_name_2))
 }
 
-co_ab_fr(dataset = df,
-         col_name_1 = result_19_20,
-         col_name_2 = result_20_21)
-
-## 6 - Comparative of the relative frequency for the a variable in different moments
+#' co_rl_fr
+#'
+#' @param dataset the dataset for which you wish to retrieve comparative 
+#' relative frequency.
+#' @param col_name_1 the first column name for which you wish to retrieve 
+#' comparative relative frequency.
+#' @param col_name_2 the second column name for which you wish to retrieve 
+#' comparative relative frequency.
+#'
+#' @return returns the comparative of the relative frequency for the a variable 
+#' in different moments.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#'co_rl_fr(dataset = soccer,
+#'         col_name_1 = result_19_20,
+#'         col_name_2 = result_20_21)}
 co_rl_fr <- function(dataset, col_name_1, col_name_2) {
   col_name_1 <- dplyr::enquo(col_name_1)
   col_name_2 <- dplyr::enquo(col_name_2)
   values <- dataset |> dplyr::group_by(!!col_name_1) |>
-    dplyr::summarise(n = n()) |> dplyr::select(-2) |>
+    dplyr::summarise(n = dplyr::n()) |> dplyr::select(-2) |>
     dplyr::rename(values = !!col_name_1)
   
   dataset |>
@@ -110,11 +153,22 @@ co_rl_fr <- function(dataset, col_name_1, col_name_2) {
     dplyr::mutate(dif_abs = abs(!!col_name_1-!!col_name_2))
 }
 
-co_rl_fr(dataset = df,
-         col_name_1 = result_19_20,
-         col_name_2 = result_20_21)
-
-## 7 - Barplot for the a variable in different moments
+#' plot_bar
+#'
+#' @param dataset the dataset for which you wish to retrieve the bar plot.
+#' @param col_name_1 the first column name for which you wish to retrieve the 
+#' bar plot.
+#' @param col_name_2 the second column name for which you wish to retrieve the 
+#' bar plot.
+#'
+#' @return returns the bar plot for the a variable in different moments.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#'plot_bar(dataset = df,
+#'         col_name_1 = result_19_20,
+#'         col_name_2 = result_20_21)}
 plot_bar <- function(dataset, col_name_1, col_name_2) {
   col_name_1 <- dplyr::enquo(col_name_1)
   col_name_2 <- dplyr::enquo(col_name_2)
@@ -142,11 +196,23 @@ plot_bar <- function(dataset, col_name_1, col_name_2) {
   
 }
 
-plot_bar(dataset = df,
-         col_name_1 = result_19_20,
-         col_name_2 = result_20_21)
-
-## 8 - Total variation distance for a categorical variable
+#' tv_d
+#'
+#' @param dataset the dataset for which you wish to retrieve the total variation 
+#' distance.
+#' @param col_name_1 the first column name for which you wish to retrieve the 
+#' total variation distance.
+#' @param col_name_2 the second column name for which you wish to retrieve the 
+#' total variation distance.
+#'
+#' @return returns the total variation distance for a categorical variable.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#'tv_d(dataset = df,
+#'     col_name_1 = result_19_20,
+#'     col_name_2 = result_20_21)}
 tv_d <- function(dataset, col_name_1, col_name_2) {
   col_name_1 <- dplyr::enquo(col_name_1)
   col_name_2 <- dplyr::enquo(col_name_2)
@@ -159,7 +225,3 @@ tv_d <- function(dataset, col_name_1, col_name_2) {
     dplyr::mutate(dif_abs = abs(!!col_name_1-!!col_name_2)) |>
     dplyr::summarise(tv = sum(dif_abs) / 2)
 }
-
-tv_d(dataset = df,
-     col_name_1 = result_19_20,
-     col_name_2 = result_20_21)
